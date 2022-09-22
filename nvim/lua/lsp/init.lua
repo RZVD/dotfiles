dc72b3e5d44ef2290549ccd79a4d258f68c76cc2
RZVD <63261141+RZVD@users.noreply.github.com>
2022-09-22 15:22:18 +0300

    Initial neovim config

---
+++ nvim/lua/lsp/init.lua
@@ -0,0 +1,147 @@
+local lsp = require('lsp-zero')
+
+lsp.set_preferences({
+    suggest_lsp_servers = true,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = true,
+    configure_diagnostics = true,
+    cmp_capabilities = true,
+    manage_nvim_cmp = true,
+    call_servers = 'local',
+    sign_icons = {
+        error = '✘',
+        warn = '▲',
+        hint = '⚑',
+        info = ''
+    }
+})
+
+--lsp.preset('recommended')
+
+--local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.7.4/'
+--local codelldb_path = extension_path .. 'adapter/codelldb'
+--local liblldb_path = extension_path .. 'lldb/lib/liblldb.so'
+
+local rust_lsp = lsp.build_options('rust_analyzer', {
+    dap = {
+        adapter = require('rust-tools.dap').get_codelldb_adapter(
+            codelldb_path, liblldb_path)
+    }
+})
+
+lsp.setup()
+require('rust-tools').setup({ server = rust_lsp })
+--nullls
+local null_ls = require "null-ls"
+
+null_ls.setup {
+    sources = {
+        null_ls.builtins.formatting.autopep8,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.prettierd,
+    },
+}
+
+vim.diagnostic.config({ -- not sure if i like virt text or not yet
+    virtual_text = true,
+})
+
+-- Debbuging
+
+
+local dap = require('dap')
+
+dap.adapters.lldb = {
+    type = 'executable',
+    command = '~/.vscode/extensions/vadimcn.vscode-lldb-1.7.4/',
+    name = 'lldb'
+}
+
+dap.configurations.cpp = {
+    {
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+
+        -- 💀
+        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+        --
+        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+        --
+        -- Otherwise you might get the following error:
+        --
+        --    Error on launch: Failed to attach to the target process
+        --
+        -- But you should be aware of the implications:
+        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+        -- runInTerminal = false,
+    },
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+--Debbuging ui
+require("dapui").setup({
+    icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+    mappings = {
+        -- Use a table to apply multiple mappings
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+    },
+    -- Expand lines larger than the window
+    -- Requires >= 0.7
+    expand_lines = vim.fn.has("nvim-0.7"),
+    -- Layouts define sections of the screen to place windows.
+    -- The position can be "left", "right", "top" or "bottom".
+    -- The size specifies the height/width depending on position. It can be an Int
+    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+    -- Elements are the elements shown in the layout (in order).
+    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+    layouts = {
+        {
+            elements = {
+                -- Elements can be strings or table with id and size keys.
+                { id = "scopes", size = 0.25 },
+                "breakpoints",
+                "stacks",
+                "watches",
+            },
+            size = 40, -- 40 columns
+            position = "left",
+        },
+        {
+            elements = {
+                "repl",
+                "console",
+            },
+            size = 0.25, -- 25% of total lines
+            position = "bottom",
+        },
+    },
+    floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1.
+        max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = "single", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+            close = { "q", "<Esc>" },
+        },
+    },
+    windows = { indent = 1 },
+    render = {
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
+    }
+})
---
+++ nvim/lua/settings/init.lua
@@ -0,0 +1,34 @@
+local set = vim.opt
+-- editing
+set.expandtab = true
+set.smarttab = true
+set.shiftwidth = 4
+set.tabstop = 4
+
+-- searching
+set.hlsearch = true
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.wrap = true
+set.scrolloff = 5
+set.fileencoding = 'utf-8'
+set.termguicolors = true
+
+-- ui
+set.number = true
+set.rnu = true
+set.cursorline = true
+
+set.hidden = true
+set.swapfile = false
+set.clipboard = 'unnamedplus'
+
+-- mouse
+set.mouse = 'a'
+
+  set.whichwrap:append "<,>,[,],h,l"
+
