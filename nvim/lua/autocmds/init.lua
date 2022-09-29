1ff5e64db688514fe9f2bf7dfd3ee984aad9c17c
RZVD <63261141+RZVD@users.noreply.github.com>
2022-09-29 22:53:28 +0300

    Delete nvim directory

--- nvim/lua/autocmds/init.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd(
-    'BufWrite',
-    {
-        command = "LspZeroFormat"
-    }
-
-)
-
-
--- nvim/lua/toggleterm-config/init.lua
+++
@@ -1,8 +0,0 @@
-require("toggleterm").setup({
-    direction = 'float',
-    persist_mode = true,
-    auto_scroll = true,
-    float_opts = {
-        border = 'curved'
-    }
-})
